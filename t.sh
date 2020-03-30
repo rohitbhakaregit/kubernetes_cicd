bc84451b80fa8bc85e888fd1b2b196920bd8ba5e
rohitbhakaregit <bhakarerohit@gmail.com>
2020-03-30 16:00:52 +0530

    removed the file

--- t.sh
+++
@@ -1,7 +0,0 @@
-echo "# kubernetes_cicd" >> README.md
-git init
-git add .
-git commit -m "first commit"
-git remote add origin https://github.com/rohitbhakaregit/kubernetes_cicd.git
-git push -u origin master
-                
