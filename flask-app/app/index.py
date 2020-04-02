c940059c4d50d434765b04d28a7f6a819988daf2
rohitbhakaregit <bhakarerohit@gmail.com>
2020-04-02 19:42:37 +0530

    added the code python flask

--- flask-app/app/index.py
+++ flask-app/app/index.py
@@ -1,29 +1,10 @@
-from flask import Flask, render_template, request
-from flask_mysqldb import MySQL
+from flask import Flask, render_template
+
 app = Flask(__name__)
-
-
-app.config['MYSQL_HOST'] = 'mydb.cmsdtwqyqzhn.us-east-1.rds.amazonaws.com'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'ramchandra'
-app.config['MYSQL_DB'] = 'MyDB'
-
-mysql = MySQL(app)
-
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    if request.method == "POST":
-        details = request.form
-        firstName = details['fname']
-        lastName = details['lname']
-        cur = mysql.connection.cursor()
-        cur.execute("INSERT INTO MyUsers(firstName, lastName) VALUES (%s, %s)", (firstName, lastName))
-        mysql.connection.commit()
-        cur.close()
-        return 'success'
     return render_template('index.html')
-
 
 if __name__ == '__main__':
     app.run()
